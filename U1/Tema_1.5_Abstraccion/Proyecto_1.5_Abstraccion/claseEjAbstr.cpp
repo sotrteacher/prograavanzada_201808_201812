420ce03e66c137da71a973cb17cf1e706c92fd6e
sotrteacher <42185871+sotrteacher@users.noreply.github.com>
2018-09-18 10:18:00 -0500

    Add files via upload

---
+++ U1/Tema_1.5_Abstraccion/Proyecto_1.5_Abstraccion/claseEjAbstr.cpp
@@ -0,0 +1,19 @@
+/**
+ * claseEjAbstr.cpp
+ */
+ 
+ #include "claseEjAbstr.h"
+ 
+ 
+ 
+ ClaseEjAbstr::ClaseEjAbstr(int cantidad){
+ 	//this->cantidad=cantidad;
+ 	cantidadArr[0]=cantidad;
+ }
+ 
+ int ClaseEjAbstr::get_cantidad(){
+ 	//return cantidad;
+ 	return cantidadArr[0];
+ }
+ 
+ 
---
+++ U1/Tema_1.5_Abstraccion/Proyecto_1.5_Abstraccion/claseEjAbstr.h
@@ -0,0 +1,16 @@
+/**
+ * claseEjAbstr.h
+ */
+ 
+ 
+ 
+ 
+ class ClaseEjAbstr{
+ 	//int cantidad;
+ 	int  cantidadArr[2];
+ 	
+public:
+	int get_cantidad(); 		
+ 	ClaseEjAbstr(int);
+ };//end class ClaseEjAbstr
+ 
